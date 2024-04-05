23d5f82694dbcf8a4931442c805a0093d3d85975
Kophi <esselenock888@gmail.com>
2024-04-05 18:04:43 -0400

    new terraform code

---
+++ import-code/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_instance.web
+  id = "i-0331ddd1ab4220e86"
+}
---
+++ meta-argument/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  
+  region = "us-east-1"
+}
+/*
+resource "aws_iam_group" "grp1" {
+    name = "jollof08"
+    lifecycle {
+      prevent_destroy = false
+    }
+}
+
+resource "aws_iam_user" "usr1" {
+    name = "Enock"
+    depends_on = [ aws_iam_group.grp1 ]
+  
+}
+*/
+
+resource "aws_instance" "seva1" {
+    ami = "ami-033a1ebf088e56e81"
+    instance_type = "t3.small"
+    key_name = "wordpress"
+    lifecycle {
+      create_before_destroy = true
+    }
+}
